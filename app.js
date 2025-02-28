064adb6d8fc9fdf3da5f7050795d7618847ec290
Madrilena <lazarmarcu@gmail.com>
2025-02-28 15:36:15 +0200

    add new function

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const subject = ['The cat','A teacher','My friend','The sun','A scientist','The dog','An artist','The president','A musician','The bird'];
-const predicate = ['eats','writes','sings','runs','paints','solves','watches','builds','studie','tells'];
+const predicate = ['eats','writes','sings','runs','paints','solves','watches','builds','studies','tells'];
 const object = ['the cake','a novel','a melody','a movie','a building','a masterpiece','the lesson','an adventure'];
 
 const randomize = arr => {
   let arrIndex = Math.floor(Math.random() * arr.length);
     return arr[arrIndex];
 }
-console.log(randomize(subject))
+const randomMessage =(sub,pred,obj) => {
+    randomize(sub);
+    randomize(pred);
+    randomize(obj);
+    console.log(randomize(sub),randomize(pred),randomize(obj))
+}
+randomMessage(subject,predicate,object);
