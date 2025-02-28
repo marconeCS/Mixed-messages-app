9d81f9ea237cfe33045fea5219608ae7c758a9f2
Madrilena <lazarmarcu@gmail.com>
2025-02-28 15:13:39 +0200

    add the arrays and randomize function

---
+++ app.js
@@ -0,0 +1,9 @@
+const subject = ['The cat','A teacher','My friend','The sun','A scientist','The dog','An artist','The president','A musician','The bird'];
+const predicate = ['eats','writes','sings','runs','paints','solves','watches','builds','studie','tells'];
+const object = ['the cake','a novel','a melody','a movie','a building','a masterpiece','the lesson','an adventure'];
+
+const randomize = arr => {
+  let arrIndex = Math.floor(Math.random() * arr.length);
+    return arr[arrIndex];
+}
+console.log(randomize(subject))
